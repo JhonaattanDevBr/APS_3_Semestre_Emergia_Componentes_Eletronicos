07cd8ce47dd38749730b24d135f577fdaf29704a
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-05-16 15:20:53 -0300

    Subindo funcionalidade completa da tela de Ajuda do usuario

--- Calculadora.View/Calculadora.View/Interfaces/FmAjuda.Designer.cs
+++ Calculadora.View/Calculadora.View/Interfaces/FmAjuda.Designer.cs
@@ -28,14 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            PbImagemAjuda = new PictureBox();
+            BtnAnterior = new Button();
+            BtnProximo = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)PbImagemAjuda).BeginInit();
             SuspendLayout();
+            // 
+            // PbImagemAjuda
+            // 
+            PbImagemAjuda.Image = Properties.Resources.Ajuda_Ciclo;
+            PbImagemAjuda.Location = new Point(12, 12);
+            PbImagemAjuda.Name = "PbImagemAjuda";
+            PbImagemAjuda.Size = new Size(710, 430);
+            PbImagemAjuda.SizeMode = PictureBoxSizeMode.StretchImage;
+            PbImagemAjuda.TabIndex = 0;
+            PbImagemAjuda.TabStop = false;
+            // 
+            // BtnAnterior
+            // 
+            BtnAnterior.Font = new Font("Arial Narrow", 12F, FontStyle.Bold);
+            BtnAnterior.Location = new Point(566, 448);
+            BtnAnterior.Name = "BtnAnterior";
+            BtnAnterior.Size = new Size(75, 30);
+            BtnAnterior.TabIndex = 1;
+            BtnAnterior.Text = "Anterior";
+            BtnAnterior.UseVisualStyleBackColor = true;
+            BtnAnterior.Click += BtnAnterior_Click;
+            // 
+            // BtnProximo
+            // 
+            BtnProximo.Font = new Font("Arial Narrow", 12F, FontStyle.Bold);
+            BtnProximo.Location = new Point(647, 448);
+            BtnProximo.Name = "BtnProximo";
+            BtnProximo.Size = new Size(75, 30);
+            BtnProximo.TabIndex = 2;
+            BtnProximo.Text = "Proximo";
+            BtnProximo.UseVisualStyleBackColor = true;
+            BtnProximo.Click += BtnProximo_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 453);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Intruções de uso do sistema.";
             // 
             // FmAjuda
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(584, 461);
+            ClientSize = new Size(734, 487);
+            Controls.Add(label1);
+            Controls.Add(BtnProximo);
+            Controls.Add(BtnAnterior);
+            Controls.Add(PbImagemAjuda);
             Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
@@ -44,9 +95,17 @@
             StartPosition = FormStartPosition.CenterScreen;
             Tag = "";
             Text = "AJUDA";
+            Load += FmAjuda_Load;
+            ((System.ComponentModel.ISupportInitialize)PbImagemAjuda).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox PbImagemAjuda;
+        private Button BtnAnterior;
+        private Button BtnProximo;
+        private Label label1;
     }
 }
--- Calculadora.View/Calculadora.View/Interfaces/FmAjuda.cs
+++ Calculadora.View/Calculadora.View/Interfaces/FmAjuda.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Calculadora.View.Interfaces
 {
-    public partial class FmAjuda: Form
+    public partial class FmAjuda : Form
     {
+        List<System.Drawing.Image> imagens;
+        int indice = 0;
+
         public FmAjuda()
         {
             InitializeComponent();
+            imagens = new List<System.Drawing.Image>
+            {
+                Properties.Resources.Ajuda_Texto,
+                Properties.Resources.Ajuda_Ciclo
+            };
+        }
+
+        private void FmAjuda_Load(object sender, EventArgs e)
+        {
+            PbImagemAjuda.SizeMode = PictureBoxSizeMode.StretchImage;
+            PbImagemAjuda.Image = imagens[indice];
+        }
+
+        private void BtnProximo_Click(object sender, EventArgs e)
+        {
+            if (indice < imagens.Count - 1)
+            {
+                indice++;
+                PbImagemAjuda.Image = imagens[indice];
+            }
+        }
+
+        private void BtnAnterior_Click(object sender, EventArgs e)
+        {
+            if (indice > 0)
+            {
+                indice--;
+                PbImagemAjuda.Image = imagens[indice];
+            }
         }
     }
 }
